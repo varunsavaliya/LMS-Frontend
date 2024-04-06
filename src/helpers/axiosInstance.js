e46992f164e407f2adc0754e5cb91e00190f07b5
varun <varunsavaliya000@gmail.com>
2024-04-06 10:41:40 +0530

    added config to axios for cookies

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = `${import.meta.env.VITE_API_URL}/api/v1`;
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create({withCredentials: true});
 
 axiosInstance.defaults.baseURL = BASE_URL;
 axiosInstance.defaults.withCredentials = true;
